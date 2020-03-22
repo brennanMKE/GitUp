61fe109c7b7817388a22221df1a888ae6c64cee6
Lucas Derraugh <lucas@derraugh.com>
2020-03-22 14:51:02 -0700

    Remove deprecated HockeySDK
    Microsoft's App Center would be a proper replacement if we wanted to track app statistics.

--- GitUp/Application/AppDelegate.m
+++ GitUp/Application/AppDelegate.m
@@ -15,7 +15,6 @@
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-interface-ivars"
-#import <HockeySDK/HockeySDK.h>
 #pragma clang diagnostic pop
 #import <Sparkle/Sparkle.h>
 
